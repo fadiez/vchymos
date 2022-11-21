c079fbc5bc4ffc4c1efca424ad390dacf9b6f7d8
fadiez <illiaforwork@gmail.com>
2022-11-21 18:11:08 +0200

    start

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+// document.write("<h1>rusni pyzda</h1>")
+// document.write("<h2>(c) pan Taras</h2>")
+
+// let a;
+// // console.log("result: "+a);
+// a = 1;
+// b = 0;
+// a = b || "aaaaaaa";
+// console.log(a)
